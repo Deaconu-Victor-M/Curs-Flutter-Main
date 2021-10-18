6746044e6664f72bf089838e4bce8b4b29857bf8
David Pescariu <davidpescariu12@gmail.com>
2021-10-18 09:08:16 +0300

    feat: Categories

--- lib/components/productList.dart
+++ lib/components/productList.dart
@@ -1,66 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:shoeapp/models/sneaker.dart';
-import 'package:shoeapp/services/apiService.dart';
 import 'package:shoeapp/widgets/shoecard.dart';
 
 class ProductList extends StatefulWidget {
-  const ProductList({Key? key}) : super(key: key);
+  const ProductList({Key? key, required this.sneakers}) : super(key: key);
+
+  final List<Sneaker>? sneakers;
 
   @override
   _ProductListState createState() => _ProductListState();
 }
 
 class _ProductListState extends State<ProductList> {
-  List<Sneaker>? _sneakers = [];
-
-  Future<void> _getData() async {
-    print('Getting data...');
-    _sneakers = await APIService.getAllSneakers();
-    print('Done!');
-  }
-
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: _getData(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState != ConnectionState.done) {
-          return Center(
-            child: Padding(
-              padding: const EdgeInsets.all(36.0),
-              child: CircularProgressIndicator(),
-            ),
-          );
-        } else {
-          if (_sneakers == null) {
-            return Center(
-              child: Padding(
-                padding: const EdgeInsets.all(36.0),
-                child: Text(
-                  "Oops, Failed to get data :(",
-                  style: TextStyle(fontSize: 24),
-                ),
-              ),
-            );
-          } else {
-            return Expanded(
-              child: ListView.builder(
-                physics: BouncingScrollPhysics(),
-                itemCount: _sneakers?.length,
-                itemBuilder: buildListView,
-              ),
-            );
-          }
-        }
-      },
-    );
+    if (widget.sneakers == null) {
+      return Center(
+        child: Padding(
+          padding: const EdgeInsets.all(36.0),
+          child: Text(
+            "Oops, Failed to get data :(",
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+      );
+    } else {
+      return Expanded(
+        child: ListView.builder(
+          physics: BouncingScrollPhysics(),
+          itemCount: widget.sneakers?.length,
+          itemBuilder: buildListView,
+        ),
+      );
+    }
   }
 
   Widget buildListView(BuildContext context, int index) {
-    if (_sneakers == null) {
+    if (widget.sneakers == null) {
       return Text('fail.');
     }
 
-    return ShoeCard(sneaker: _sneakers![index]);
+    return ShoeCard(sneaker: widget.sneakers![index]);
   }
 }
--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:shoeapp/components/appBar.dart';
 import 'package:shoeapp/components/productList.dart';
+import 'package:shoeapp/models/sneaker.dart';
+import 'package:shoeapp/services/apiService.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,13 +12,47 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int selectedIndex = 0;
-  List category = [
+  int _selectedIndex = 0;
+  List _category = [
     'All',
     'Adidas',
     'Nike',
     'Reebok',
   ];
+
+  bool _isLoading = true;
+  List<Sneaker>? _sneakers = [];
+
+  @override
+  void initState() {
+    super.initState();
+    print("----- Getting sneakers -----");
+    APIService.getAllSneakers().then((sneakers) {
+      _sneakers = sneakers;
+      _isLoading = false;
+      setState(() {});
+      print("----- Done -----");
+    });
+  }
+
+  List<Sneaker>? _getSneakersForCategory(String category) {
+    if (_sneakers == null) {
+      return null;
+    }
+
+    if (category == "All") {
+      return _sneakers;
+    }
+
+    List<Sneaker> sortedList = [];
+    _sneakers!.forEach((sneaker) {
+      if (sneaker.brand == category.toLowerCase()) {
+        sortedList.add(sneaker);
+      }
+    });
+    return sortedList;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,71 +60,80 @@ class _HomePageState extends State<HomePage> {
       extendBody: false,
       backgroundColor: Color(0xFFF5F5F5),
       appBar: CustomAppBar(),
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 20, top: 20),
-              child: Text(
-                'Discover \nshoes weekly',
-                style: TextStyle(
-                  fontFamily: 'Inter',
-                  color: Color(0xFF6C6DDE),
-                  fontSize: 45,
-                  fontWeight: FontWeight.w400,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 20),
-              child: Container(
-                height: 40,
-                width: MediaQuery.of(context).size.width,
-                child: ListView.builder(
-                  physics: BouncingScrollPhysics(
-                    parent: AlwaysScrollableScrollPhysics(),
+      body: _isLoading
+          ? CircularProgressIndicator()
+          : SafeArea(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20, top: 20),
+                    child: Text(
+                      'Discover \nshoes weekly',
+                      style: TextStyle(
+                        fontFamily: 'Inter',
+                        color: Color(0xFF6C6DDE),
+                        fontSize: 45,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
                   ),
-                  scrollDirection: Axis.horizontal,
-                  itemCount: category.length,
-                  itemBuilder: (context, i) {
-                    return GestureDetector(
-                      onTap: () {
-                        setState(
-                          () {
-                            selectedIndex = i;
-                          },
-                        );
-                      },
-                      child: Container(
-                        padding: const EdgeInsets.only(left: 45, right: 15),
-                        child: Column(
-                          children: [
-                            Text(
-                              category[i],
-                              style: TextStyle(
-                                fontFamily: 'Inter',
-                                color: Color(0xFF000000),
-                                fontSize: 25,
-                                fontWeight: selectedIndex == i
-                                    ? FontWeight.bold
-                                    : FontWeight.w400,
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20),
+                    child: Container(
+                      height: 40,
+                      width: MediaQuery.of(context).size.width,
+                      child: ListView.builder(
+                        physics: BouncingScrollPhysics(
+                          parent: AlwaysScrollableScrollPhysics(),
+                        ),
+                        scrollDirection: Axis.horizontal,
+                        itemCount: _category.length,
+                        itemBuilder: (context, i) {
+                          return GestureDetector(
+                            onTap: () {
+                              setState(
+                                () {
+                                  _selectedIndex = i;
+                                },
+                              );
+                            },
+                            child: Container(
+                              padding: const EdgeInsets.only(
+                                left: 45,
+                                right: 15,
+                              ),
+                              child: Column(
+                                children: [
+                                  Text(
+                                    _category[i],
+                                    style: TextStyle(
+                                      fontFamily: 'Inter',
+                                      color: Color(0xFF000000),
+                                      fontSize: 25,
+                                      fontWeight: _selectedIndex == i
+                                          ? FontWeight.bold
+                                          : FontWeight.w400,
+                                    ),
+                                  ),
+                                ],
                               ),
                             ),
-                          ],
-                        ),
+                          );
+                        },
                       ),
-                    );
-                  },
-                ),
+                    ),
+                  ),
+
+                  /// "Main" product list
+                  ProductList(
+                    sneakers: _getSneakersForCategory(
+                      _category[_selectedIndex],
+                    ),
+                  ),
+                ],
               ),
             ),
-
-            /// "Main" product list
-            ProductList(),
-          ],
-        ),
-      ),
     );
   }
 }
